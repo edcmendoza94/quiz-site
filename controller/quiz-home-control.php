4c91f75ba90961ce92c2925b51bcdd77f81d2ff8
Edward Mendoza <edcmendoza94@gmail.com>
2017-06-10 00:36:19 -0500

    Quizzes and number of questions in quizzes display properly

--- controller/quiz-home-control.php
+++ controller/quiz-home-control.php
@@ -7,6 +7,37 @@
  */
 class QuizHomeControl
 {
+    private $_f3;
+    private $_params;
     
+    public function __construct($f3, $params)
+    {
+        $this->_f3 = $f3;
+        $this->_params = $params;
+    }
+    
+    public function viewHome()
+    {
+        $db = new QuizSiteDB();
+        
+        $quizzes = $db->showAllQuizzes();
+        $quizArr = array();
+        
+        foreach($quizzes as $quiz){
+            $thisQuiz = new Quiz($quiz['quiz_id'], $quiz['quiz_name'],
+                                 $quiz['num_of_questions']);
+            $quizArr[] = $thisQuiz;
+        }
+        
+        //var_dump($quizArr);
+        
+        $numOfQuizzes = sizeof($quizArr);
+        
+        $this->_f3->set('quizzes', $quizArr);
+        $this->_f3->set('numOfQuizzes', $numOfQuizzes);
+        
+        echo Template::instance()->render('view/include/header.html');
+        echo Template::instance()->render('view/quiz-home.html');
+    }
 }
 ?>
